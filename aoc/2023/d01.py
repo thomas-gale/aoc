8c6a31a165302fe8f00da4f1ef4ec2bddcb2c074
Thomas Gale <thomas.gale@autodesk.com>
2024-03-12 14:01:08 +0000

    2023 d1 complete.

--- aoc/2023/d01.py
+++ aoc/2023/d01.py
@@ -2,9 +2,47 @@ from aoc.util import load_input_lines
 
 
 # Part 1.
-def p1():
-    ls = load_input_lines(2023, 1, "t")
-    print(ls)
+def p1(ls):
+    sum = 0
+    for l in ls:
+        digits = [int(c) for c in l if c.isdigit()]
+        sum += digits[0] * 10 + digits[-1]
+    print(sum)
 
 
-p1()
+# Part 2
+import re
+
+nums = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+
+def p2(ls):
+    sum = 0
+    for l in ls:
+        # Get digit matches and locations (same as part 1)
+        digits = {i: int(c) for i, c in enumerate(l) if c.isdigit()}
+
+        # Get string num matches and locations
+        for n in nums.keys():
+            for m in re.finditer(n, l):
+                digits[m.span()[0]] = nums[n]
+
+        sk = sorted(digits.keys())
+        sum += digits[sk[0]] * 10 + digits[sk[-1]]
+
+    print(sum)
+
+
+ls = load_input_lines(2023, 1)
+p1(ls)
+p2(ls)
