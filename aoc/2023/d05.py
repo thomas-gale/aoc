0eb270b016c354d59f12fa27fa6ca0c07ec5f7fb
Thomas Gale <thomas.gale@autodesk.com>
2024-03-28 18:50:28 +0000

    Part 1 completed, part 2 brute force too slow.

--- aoc/2023/d05.py
+++ aoc/2023/d05.py
@@ -1,10 +1,13 @@
-import re
 from collections import defaultdict
+from itertools import chain
+import re
 from aoc.util import load_input_lines
 
 
-def p1(ls):
-    seeds = map(int, ls[0].split(":")[1].strip().split())
+def get_maps(ls):
+    """
+    Maps of source to (destination and length)
+    """
     ms = []
     m = {}
     for l in ls[2:]:
@@ -14,24 +17,42 @@ def p1(ls):
         mat = re.match(r"(\d+) (\d+) (\d+)", l)
         if mat:
             r = list(map(int, mat.group(0).split()))
-            m[r[0]] = r
-            
-    def mapper(x: int, m: dict):
-        for i in sorted(m.keys()):
-            if x < i:
-                return x,
-            # if x < m[i]
-            # k
-            
+            m[r[1]] = (r[0], r[2])
+    ms.append(m)
+    return ms
 
-    # print(ms)
 
-    # print(list(seeds))
-    return None
+def mapper(x: int, m: dict):
+    for i in sorted(m.keys()):
+        if x >= i and x < i + m[i][1]:
+            return m[i][0] + x - i
+    return x
+
+
+def p1(ls):
+    seeds = map(int, ls[0].split(":")[1].strip().split())
+    ms = get_maps(ls)
+    locs = []
+    for x in seeds:
+        for m in ms:
+            x = mapper(x, m)
+        locs.append(x)
+    return sorted(locs)[0]
 
 
 def p2(ls):
-    pass
+    seeds = list(map(int, ls[0].split(":")[1].strip().split()))
+    seeds = zip(seeds[::2], seeds[1::2])
+    seeds = list(chain(*[list(range(x, x + y)) for x, y in seeds]))
+    print(list(seeds))
+    ms = get_maps(ls)
+    locs = []
+    # Brute force too slow
+    for x in seeds:
+        for m in ms:
+            x = mapper(x, m)
+        locs.append(x)
+    return sorted(locs)[0]
 
 
 ls = load_input_lines(2023, 5, "t")
